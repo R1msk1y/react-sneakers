233943d3bc5c961b10f710777899df22a1667983
R1msk1y <wkurganow@gmail.com>
2022-08-19 03:25:47 +0700

    начал верстать шапку

--- src/App.js
+++ src/App.js
@@ -3,10 +3,40 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello world!</h1>
-      <p>Новый курс для джунов</p>
-    
+    <div className="wrapper">
+    <header>
+    <div className='headerLeft'>
+
+    <img width={40} height={40} src='/img/header-logo.png'/>
+      <div className='headerInfo'>
+        <h3> React Sneakers</h3>
+        <p>Магазин лучших кросовок</p>
+      </div>
+
+    </div> 
+    // left part END
+
+  
+          
+    <ul className='headerRight'>
+
+<li>
+  <svg></svg> <span>1205 руб.</span>
+  </li>
+
+<li>
+  <svg></svg>
+  </li>
+
+</ul>
+   
+    // right part END
+
+    </header>
+
+    <div className='content'>
+      <h1>Все кросовки</h1>
+    </div>
 
     </div>
   );
