b954a8e00c3012844932a3d3917506be2c4c10e6
R1msk1y <wkurganow@gmail.com>
2022-08-21 02:02:22 +0700

    почти закончил верстку основной странитцы, в частности сделал выплывашку, aka корзину

--- src/App.js
+++ src/App.js
@@ -4,6 +4,72 @@ import './App.css';
 function App() {
   return (
     <div className="wrapper clear">
+
+      <div className='overlay'>
+        <div className='drawer'>
+          <h2 className='mb-30'>Корзина</h2>
+
+          <div className='items'>
+
+          <div className='cartItem d-flex align-center mb-20'>
+            
+            <div className='cartItemImg' style={{backgroundImage: 'url(/img/sneakers/product1.jpg)'}} >
+            </div>
+            <div className='mr-20 flex'>
+              <p className='mb-5'>Мужские Кроссовки Nike Air Max 270</p>
+              <b>12 999 руб.</b>   
+            </div>
+            <img className='removeBtn' src='/img/btn-remove.svg' alt='btn-remove'/>
+          </div>
+
+          <div className='cartItem d-flex align-center mb-20'>
+            
+            <div className='cartItemImg' style={{backgroundImage: 'url(/img/sneakers/product1.jpg)'}} >
+            </div>
+            <div className='mr-20 flex'>
+              <p className='mb-5'>Мужские Кроссовки Nike Air Max 270</p>
+              <b>12 999 руб.</b>   
+            </div>
+            <img className='removeBtn' src='/img/btn-remove.svg' alt='btn-remove'/>
+          </div>
+
+
+
+
+          </div>
+
+          <div className='cartTotalBlock'>
+
+          <ul>
+            <li>
+              <span>Итого:</span>
+              <div></div>
+              <b>21 498 руб.</b>
+            </li>
+
+            <li>
+            <span>Налог 5%:</span>
+              <div></div>
+              <b>1074 руб.</b>
+            </li>
+
+          </ul>
+
+          <button className='greenBtn'>Оформить заказ<img src='/img/arrow.svg' alt='arrow' /></button>
+
+
+          </div>
+
+          
+
+       
+
+        </div>
+      </div>
+
+
+
+
     <header className='d-flex justify-between align-center p-40'>
     <div className='d-flex align-center'>
 
@@ -36,10 +102,21 @@ function App() {
     </header>
 
     <div className='content p-40'>
-      <h1 className='mb-40'>Все кросcовки</h1>
+      <div className='d-flex align-center mb-40 justify-between'>
+      <h1>Все кросcовки</h1>
+      <div className='search-block d-flex'>
+        <img src='img/search.svg' alt='search'/>
+        <input placeholder='Поиск...'/>
+
+      </div>
+      
+      </div>
 
       <div className='d-flex'>
       <div className='card'>
+        <div className='favorite'>
+        <img src='/img/likeUnliked.svg' alt='like-unliked'/>
+        </div>
         <img width={133} height={112} src='/img/sneakers/product1.jpg' alt='Sneakers' />
         <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
         <div className='d-flex justify-between align-center'>
