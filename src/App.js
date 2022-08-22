c6a36ef26881b4b6667a5c3621c771af5d1679f8
R1msk1y <wkurganow@gmail.com>
2022-08-22 23:37:36 +0700

    добавил отоброжение карточек товаров через получение данных из массива+разобрал и применил пропсы

--- src/App.js
+++ src/App.js
@@ -3,6 +3,31 @@ import './App.css';
 import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
+const arr = [
+  {
+    name: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: 12999,
+    imageUrl: '/img/sneakers/product1.jpg' 
+  },
+  {
+    name: 'Мужские Кроссовки Nike Air Max 270',
+    price: 9000,
+    imageUrl: '/img/sneakers/product2.jpg' 
+  },
+  {
+    name: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: 8499,
+    imageUrl: '/img/sneakers/product3.jpg' 
+  },
+  {
+    name: 'Кроссовки Puma X Aka Boku Future Rider',
+    price: 8999,
+    imageUrl: '/img/sneakers/product4.jpg' 
+
+  }
+
+
+];
 
 function App() {
   return (
@@ -28,7 +53,21 @@ function App() {
       </div>
 
       <div className='d-flex'>
-        <Card />
+      
+
+      {arr.map(function(item){
+
+        return <Card
+        title = {item.name}
+        price = {item.price} 
+        imageUrl = {item.imageUrl} 
+   
+        />
+
+      })}
+
+  
+      
       
 
     
